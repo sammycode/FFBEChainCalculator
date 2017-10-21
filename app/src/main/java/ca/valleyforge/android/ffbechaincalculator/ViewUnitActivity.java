d342a3900f95b8e5f1d39e548a3c1916d8fd341e
MBJ <mikejamer@gmail.com>
2017-10-21 14:52:41 -0300

    Adding View Unit Activity, this is going to be required to take a closer look at a unit.  The goal will be to shift the item click, which currently goes directly to the edit unit activity, to instead go here, allowing the user to view the unit. From here the user will be able to hit the "Edit Unit" button to get to that activity, and this will also provide the user with a way of assigning abilities to a unit.

---
+++ app/src/main/java/ca/valleyforge/android/ffbechaincalculator/ViewUnitActivity.java
@@ -0,0 +1,20 @@
+package ca.valleyforge.android.ffbechaincalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The View Unit Activity
+ */
+public class ViewUnitActivity extends AppCompatActivity {
+
+    /**
+     * Fires on Activity Create
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_unit);
+    }
+}
